dff4965f57e1fc04ecd9cd350b993c671b389a6c
Anton <khdevnet@gmail.com>
2023-03-25 15:28:09 +0000

    up

--- src/DRE.Api/Data/WeatherDbContext.cs
+++ src/DRE.Api/Data/WeatherDbContext.cs
@@ -1,12 +1,5 @@
 namespace DRE.Api.Data;
 
 public class SchoolContext : DbContext
-    {
-        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
-        {
-        }
-
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Student> Students { get; set; }
-    }
+{
+}
